4fe10e49d1e7de7dfa2a7e9cd3b0415f3fc27bed
Upsiteful <upsiteful@gmail.com>
2026-05-06 18:34:13 +0200

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,522 @@
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-vogel-noot-raonik-ws430-desni": {
+    "id": "plugovi-vogel-noot-raonik-ws430-desni",
+    "name": "RAONIK WS 430 DESNI V&N",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Vogel & Noot",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-vogel-noot"].products.push(
+  "plugovi-vogel-noot-raonik-ws430-desni"
+);
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-vogel-noot-raonik-wxhm43-levi": {
+    "id": "plugovi-vogel-noot-raonik-wxhm43-levi",
+    "name": "RAONIK V&N WXHM43 LIJEVI",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Vogel & Noot",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-vogel-noot"].products.push(
+  "plugovi-vogel-noot-raonik-wxhm43-levi"
+);
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-vogel-noot-raonik-wst-levi": {
+    "id": "plugovi-vogel-noot-raonik-wst-levi",
+    "name": "Raonik WST levi",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Vogel & Noot",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-vogel-noot"].products.push(
+  "plugovi-vogel-noot-raonik-wst-levi"
+);
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-dl": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-dl",
+    "name": "Raonik pretplužnjaka Lemken D/L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-dl"
+);
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-s150-d": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-s150-d",
+    "name": "Raonik pretplužnjaka Lemken S150 D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-s150-d"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-s150-l": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-s150-l",
+    "name": "Raonik pretplužnjaka Lemken S150 L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-s150-l"
+);
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-s185-d": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-s185-d",
+    "name": "Raonik pretplužnjaka Lemken S185 D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-s185-d"
+);
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-s185-l": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-s185-l",
+    "name": "Raonik pretplužnjaka Lemken S185 L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-s185-l"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-lemken-raonik-pretpluznjaka-s190-d": {
+    "id": "plugovi-lemken-raonik-pretpluznjaka-s190-d",
+    "name": "Raonik pretplužnjaka Lemken S190 D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Lemken",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-lemken"].products.push(
+  "plugovi-lemken-raonik-pretpluznjaka-s190-d"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-regent-raonik-du435-s-levi": {
+    "id": "plugovi-regent-raonik-du435-s-levi",
+    "name": "Raonik Regent DU 435 S levi",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Regent",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-regent"].products.push(
+  "plugovi-regent-raonik-du435-s-levi"
+);
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-regent-raonik-pretpluznjaka-desni": {
+    "id": "plugovi-regent-raonik-pretpluznjaka-desni",
+    "name": "Raonik pretplužnjaka Regent desni",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Regent",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-regent"].products.push(
+  "plugovi-regent-raonik-pretpluznjaka-desni"
+);
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-regent-raonik-pretpluznjaka-l-mgs": {
+    "id": "plugovi-regent-raonik-pretpluznjaka-l-mgs",
+    "name": "Raonik pretplužnjaka Regent L MGS",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Regent",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-regent"].products.push(
+  "plugovi-regent-raonik-pretpluznjaka-l-mgs"
+);
+
+
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-regent-raonik-dw40-s-d": {
+    "id": "plugovi-regent-raonik-dw40-s-d",
+    "name": "Raonik Regent DW 40 S D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Regent",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-regent"].products.push(
+  "plugovi-regent-raonik-dw40-s-d"
+);
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-regent-raonik-dw40-s-l": {
+    "id": "plugovi-regent-raonik-dw40-s-l",
+    "name": "Raonik Regent DW 40 S L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Regent",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-regent"].products.push(
+  "plugovi-regent-raonik-dw40-s-l"
+);
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-overum-raonik-d": {
+    "id": "plugovi-overum-raonik-d",
+    "name": "Raonik Overum D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Overum",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-overum"].products.push(
+  "plugovi-overum-raonik-d"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-imt-raonik-vn-14-16-758": {
+    "id": "plugovi-imt-raonik-vn-14-16-758",
+    "name": "Raonik V&N 14\"-16\" IMT 758",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "IMT",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-imt"].products.push(
+  "plugovi-imt-raonik-vn-14-16-758"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-kverneland-raonik-16-d": {
+    "id": "plugovi-kverneland-raonik-16-d",
+    "name": "Raonik Kverneland 16\" D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Kverneland",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-kverneland"].products.push(
+  "plugovi-kverneland-raonik-16-d"
+);
+
+
+
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-kverneland-raonik-pretpluznjaka-l": {
+    "id": "plugovi-kverneland-raonik-pretpluznjaka-l",
+    "name": "Raonik pretplužnjaka Kverneland L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Kverneland",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-kverneland"].products.push(
+  "plugovi-kverneland-raonik-pretpluznjaka-l"
+);
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-kverneland-raonik-14-levi": {
+    "id": "plugovi-kverneland-raonik-14-levi",
+    "name": "Raonik Kverneland 14\" levi",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Kverneland",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-kverneland"].products.push(
+  "plugovi-kverneland-raonik-14-levi"
+);
+
+
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-rabe-raonik-pretpluznjaka-sdg181-l": {
+    "id": "plugovi-rabe-raonik-pretpluznjaka-sdg181-l",
+    "name": "Raonik pretplužnjaka Rabe SDG 181 L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Rabewerk",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-rabewerk"].products.push(
+  "plugovi-rabe-raonik-pretpluznjaka-sdg181-l"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-rabe-raonik-pretpluznjaka-sdg18b-l": {
+    "id": "plugovi-rabe-raonik-pretpluznjaka-sdg18b-l",
+    "name": "Raonik pretplužnjaka Rabe SDG 18B L",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Rabewerk",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-rabewerk"].products.push(
+  "plugovi-rabe-raonik-pretpluznjaka-sdg18b-l"
+);
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-rabe-raonik-ssp351-l-wpl": {
+    "id": "plugovi-rabe-raonik-ssp351-l-wpl",
+    "name": "Raonik Rabe SSP 351 L WPL",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Rabewerk",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-rabewerk"].products.push(
+  "plugovi-rabe-raonik-ssp351-l-wpl"
+);
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-landsberg-raonik-l-wss": {
+    "id": "plugovi-landsberg-raonik-l-wss",
+    "name": "Raonik Landsberg L WSS",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Pöttinger-Landsberg",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-pottinger-landsberg"].products.push(
+  "plugovi-landsberg-raonik-l-wss"
+);
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-landsberg-raonik-levi": {
+    "id": "plugovi-landsberg-raonik-levi",
+    "name": "Raonik Landsberg levi",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Pottinger-Landsberg",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-pottinger-landsberg"].products.push(
+  "plugovi-landsberg-raonik-levi"
+);
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-landsberg-raonik-levi-000850z": {
+    "id": "plugovi-landsberg-raonik-levi-000850z",
+    "name": "Raonik Landsberg levi 000850Z",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Pottinger-Landsberg",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-pottinger-landsberg"].products.push(
+  "plugovi-landsberg-raonik-levi-000850z"
+);
+
+
+
+
+
+
+
+
+
+
+Object.assign(window.SITE_DATA.products, {
+  "plugovi-landsberg-raonik-pretpluznjaka-d": {
+    "id": "plugovi-landsberg-raonik-pretpluznjaka-d",
+    "name": "Raonik pretplužnjaka Landsberg D",
+    "section": "Plugovi",
+    "category": "Raonici",
+    "group": "Pottinger-Landsberg",
+    "description": ""
+  }
+});
+
+window.SITE_DATA.nodes["plugovi-raonici-pottinger-landsberg"].products.push(
+  "plugovi-landsberg-raonik-pretpluznjaka-d"
+);
